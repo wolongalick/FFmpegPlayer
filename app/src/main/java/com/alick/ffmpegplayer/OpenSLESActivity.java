160b34193b1a3e2d140d1d29fabaefd44030a778
cuixw <1607009565@qq.com>
2020-01-02 01:28:42 +0800

    首次可以播放pcm音频文件

--- app/src/main/java/com/alick/ffmpegplayer/OpenSLESActivity.java
+++ app/src/main/java/com/alick/ffmpegplayer/OpenSLESActivity.java
@@ -1,9 +1,17 @@
 package com.alick.ffmpegplayer;
 
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.alick.myplayer.utils.T;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class OpenSLESActivity extends AppCompatActivity {
 
@@ -15,6 +23,40 @@ public class OpenSLESActivity extends AppCompatActivity {
 
 
     public void playPcmByOpenSL_ES(View view) {
+
+        final String fileName="nfs.pcm";
+        try {
+            File pcmFile=new File(Environment.getExternalStorageDirectory(),fileName);
+
+            if(pcmFile.isFile() && pcmFile.exists()){
+                ByOpenSL_ES(pcmFile.getAbsolutePath());
+            }else {
+                InputStream inputStream = getAssets().open(fileName);
+                FileOutputStream outputStreamWriter=null;
+                try {
+                    outputStreamWriter   =new FileOutputStream(pcmFile);
+                    byte[] buffer=new byte[2048];
+                    int size;
+                    while ((size=inputStream.read(buffer))>0){
+                        outputStreamWriter.write(buffer,0,size);
+                        outputStreamWriter.flush();
+                    }
+                } finally {
+                    inputStream.close();
+                    if(outputStreamWriter!=null){
+                        outputStreamWriter.close();
+                    }
+                }
+
+                if(pcmFile.isFile()&& pcmFile.exists()){
+                    ByOpenSL_ES(pcmFile.getAbsolutePath());
+                }else {
+                    T.show(OpenSLESActivity.this,"nfs.pcm文件不存在");
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
--- myplayer/src/main/cpp/native-lib.cpp
+++ myplayer/src/main/cpp/native-lib.cpp
@@ -60,6 +60,44 @@ WlPlaystatus *playstatus = nullptr;
 
 SLObjectItf engineObjectItf = nullptr;
 SLEngineItf engineEngine = nullptr;
+SLObjectItf outputMixObject = nullptr;
+SLEnvironmentalReverbItf outputMixEnvironmentalReverb = nullptr;
+SLEnvironmentalReverbSettings reverbSettings = SL_I3DL2_ENVIRONMENT_PRESET_STONECORRIDOR;
+SLObjectItf pcmPlayerObject = nullptr;
+SLPlayItf pcmPlayerPlay = nullptr;
+SLAndroidSimpleBufferQueueItf pcmBufferQueue = nullptr;
+
+FILE *pcmFile = nullptr;
+void *buffer = nullptr;
+uint8_t *out_buffer = nullptr;
+
+
+SLuint32 getPcmData(void **pcm) {
+    SLuint32 size = 0;
+    while (!feof(pcmFile)) {
+        size = fread(out_buffer, 1, 44100 * 2 * 2, pcmFile);
+        if (out_buffer == nullptr) {
+            LOGE("读取结束");
+            break;
+        } else {
+            LOGI("读取中...");
+        }
+        *pcm = out_buffer;
+        break;
+    }
+
+    return size;
+
+}
+
+void pcmBufferCallback(SLAndroidSimpleBufferQueueItf buf, void *context) {
+    SLuint32 size = getPcmData(&buffer);
+    if (buffer != nullptr) {
+        (*pcmBufferQueue)->Enqueue(pcmBufferQueue, buffer, size);
+//        (*pcmBufferQueue)->Enqueue(pcmBufferQueue, buffer, 44100 * 2 * 2);
+    }
+
+}
 
 
 extern "C" {
@@ -128,6 +166,8 @@ Java_com_alick_myplayer_WlPlayer_n_1prepare(JNIEnv *env, jobject thiz, jstring s
 
 //    env->ReleaseStringUTFChars(source_,source);
 
+
+
 }
 
 /**
@@ -153,18 +193,84 @@ JNIEXPORT void JNICALL
 Java_com_alick_ffmpegplayer_OpenSLESActivity_ByOpenSL_1ES(JNIEnv *env, jobject thiz, jstring url_) {
     // TODO: implement ByOpenSL_ES()
 
-    const char *url = env->GetStringUTFChars(url_, false);
+    const char *url = env->GetStringUTFChars(url_, nullptr);
 
-    SLresult result = NULL;
-    //1.创建接口对象
-    result = slCreateEngine(&engineObjectItf, 0, nullptr, 0, nullptr, nullptr);
+    pcmFile = fopen(url, "r");
 
+    if (pcmFile == nullptr) {
+        return;
+    }
 
+    LOGI("打开文件成功:%s", url);
+
+    out_buffer = static_cast<uint8_t *>(malloc(44100 * 2 * 2));
+
+    SLresult result;
+    //1.创建引擎对象
+    LOGI("创建引擎对象");
+    slCreateEngine(&engineObjectItf, 0, nullptr, 0, nullptr, nullptr);
     (*engineObjectItf)->Realize(engineObjectItf, SL_BOOLEAN_FALSE);
     (*engineObjectItf)->GetInterface(engineObjectItf, SL_IID_ENGINE, &engineEngine);
 
-    //2.设置混音器
+    //2.创建混音器
+    LOGI("创建混音器");
+    SLuint32 numInterfaces = 1;//混音器个数
+    const SLInterfaceID mixIds[1] = {SL_IID_ENVIRONMENTALREVERB};//混音器id数组
+    const SLboolean mixReq[1] = {SL_BOOLEAN_FALSE};//
+
+    (*engineEngine)->CreateOutputMix(engineEngine, &outputMixObject, numInterfaces, mixIds, mixReq);
+    (*outputMixObject)->Realize(outputMixObject, SL_BOOLEAN_FALSE);
+    (*outputMixObject)->GetInterface(outputMixObject, SL_IID_ENVIRONMENTALREVERB,
+                                     &outputMixEnvironmentalReverb);
+
+    (*outputMixEnvironmentalReverb)->SetEnvironmentalReverbProperties(outputMixEnvironmentalReverb,
+                                                                      &reverbSettings);
+    SLDataLocator_OutputMix outputMix = {SL_DATALOCATOR_OUTPUTMIX, outputMixObject};
+
+    // 第三步--------------------------------------------
+    // 创建播放器
+    SLDataLocator_AndroidSimpleBufferQueue android_queue = {SL_DATALOCATOR_ANDROIDSIMPLEBUFFERQUEUE,
+                                                            2};
+
+    SLDataFormat_PCM pcm = {
+            SL_DATAFORMAT_PCM,
+            2,
+            SL_SAMPLINGRATE_44_1,//采样率
+            SL_PCMSAMPLEFORMAT_FIXED_16,//采样位数
+            SL_PCMSAMPLEFORMAT_FIXED_16,
+            SL_SPEAKER_FRONT_LEFT | SL_SPEAKER_FRONT_RIGHT,
+            SL_BYTEORDER_LITTLEENDIAN//结束标志
+    };
 
 
+    SLDataSource slDataSource = {&android_queue, &pcm};
+    SLDataSink audioSnk = {&outputMix, nullptr};
+    const SLInterfaceID ids[3] = {SL_IID_BUFFERQUEUE, SL_IID_EFFECTSEND, SL_IID_VOLUME};
+    const SLboolean req[3] = {SL_BOOLEAN_TRUE, SL_BOOLEAN_TRUE, SL_BOOLEAN_TRUE};
+
+
+    //创建播放器(录音器)
+    (*engineEngine)->CreateAudioPlayer(engineEngine, &pcmPlayerObject, &slDataSource, &audioSnk, 3,
+                                       ids, req);
+    //初始化播放器
+    (*pcmPlayerObject)->Realize(pcmPlayerObject, SL_BOOLEAN_FALSE);
+    //得到接口后调用  获取Player接口
+    (*pcmPlayerObject)->GetInterface(pcmPlayerObject, SL_IID_PLAY, &pcmPlayerPlay);
+
+
+    //第四步---------------------------------------
+    // 创建缓冲区和回调函数
+
+    //第四步
+    (*pcmPlayerObject)->GetInterface(pcmPlayerObject, SL_IID_BUFFERQUEUE, &pcmBufferQueue);
+
+    //4.设置缓冲队列和回调函数
+    (*pcmBufferQueue)->RegisterCallback(pcmBufferQueue, pcmBufferCallback, nullptr);
+
+    //5.设置播放状态
+    (*pcmPlayerPlay)->SetPlayState(pcmPlayerPlay, SL_PLAYSTATE_PLAYING);
+
+    //6.启动回调函数
+    pcmBufferCallback(pcmBufferQueue, nullptr);
 
 }
